a8e41b8d4e5193dd6fbc4beda45a5faffba858e0
Ollie Buck <git@olliebuck.com>
2015-11-30 16:38:20 +0000

    testing, 9 of 9

--- subjects/04-testing/exercise/modules/__tests__/Tabs-test.js
+++ subjects/04-testing/exercise/modules/__tests__/Tabs-test.js
@@ -32,25 +32,66 @@ describe('when <Tabs> is rendered', function () {
     document.body.removeChild(node)
   })
 
-  it('renders the USA tab')
+  it('renders the USA tab', function() {
+    assert(
+      tabs[0].innerText = FixtureData[0].label,
+      'USA tab renders'
+    )
+  })
 
-  it('renders the Brazil tab')
+  it('renders the Brazil tab', function() {
+    assert(
+      tabs[1].innerText = FixtureData[1].label,
+      'Brazil tab renders'
+    )
+  })
 
-  it('renders the Russia tab')
+  it('renders the Russia tab', function() {
+    assert(
+      tabs[2].innerText = FixtureData[2].label,
+      'Russia tab renders'
+    )
+  })
 
-  it('activates the first tab')
+  it('activates the first tab', function() {
+    assert(
+      tabs[0].style['border-bottom-color'] === borderFixture.style['border-bottom-color'],
+      'First tab matches active fixture'
+    );
+  })
 
-  it('does not activate the second tab')
+  it('does not activate the second tab', function() {
+    assert(
+      tabs[1].style['border-bottom-color'] !== borderFixture.style['border-bottom-color'],
+      'Second tab does not match active fixture'
+    );
+  })
+
+  it('has the correct content in the panel', function() {
+    assert(panel.innerText === FixtureData[0].content, 'Panel has expected content with tab 1 selected')
+  });
 
   describe('after clicking the second tab', function () {
     beforeEach(function () {
-      // TODO: Simulate a click event on the second tab
+      Simulate.click(tabs[1])
     })
 
-    it('activates the second tab')
+    it('activates the second tab', function() {
+      assert(
+        tabs[1].style['border-bottom-color'] === borderFixture.style['border-bottom-color'],
+        'Second tab does match active fixture'
+      );
+    })
 
-    it('deactivates the first tab')
+    it('deactivates the first tab', function() {
+      assert(
+        tabs[0].style['border-bottom-color'] !== borderFixture.style['border-bottom-color'],
+        'First tab does not match active fixture'
+      );
+    })
 
-    it('puts the correct content in the panel')
+    it('puts the correct content in the panel', function() {
+      assert(panel.innerText === FixtureData[1].content, 'Panel has expected content with tab 2 selected')
+    });
   })
 })
