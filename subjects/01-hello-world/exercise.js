935e551bce9c943fc3165d6c47c46e6ef1e6209b
Ollie Buck <git@olliebuck.com>
2015-11-30 11:04:11 +0000

    exercise 1

--- subjects/01-hello-world/exercise.js
+++ subjects/01-hello-world/exercise.js
@@ -8,7 +8,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-  return <div>Hello world!</div>
+  return <div>waaaatchify!</div>
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
