70af7ecadf6d6d5d75dbff3e8378d85d05aceb45
Ollie Buck <git@olliebuck.com>
2015-11-30 15:20:27 +0000

    exercise 5

--- subjects/05-props-v-state/exercise.js
+++ subjects/05-props-v-state/exercise.js
@@ -23,22 +23,18 @@ import data from './lib/data'
 const Tabs = React.createClass({
 
   propTypes: {
-    data: React.PropTypes.array.isRequired
-  },
-
-  getInitialState() {
-    return {
-      activeTabIndex: 0
-    }
+    data: React.PropTypes.array.isRequired,
+    activeIndex: React.PropTypes.number,
+    onTabChange: React.PropTypes.func.isRequired
   },
 
   handleTabClick(activeTabIndex) {
-    this.setState({ activeTabIndex })
+    this.props.onTabChange(activeTabIndex)
   },
 
   renderTabs() {
     return this.props.data.map((tab, index) => {
-      const style = this.state.activeTabIndex === index ?
+      const style = this.props.activeIndex === index ?
         styles.activeTab : styles.tab
       return (
         <div
@@ -52,7 +48,7 @@ const Tabs = React.createClass({
   },
 
   renderPanel() {
-    const tab = this.props.data[this.state.activeTabIndex]
+    const tab = this.props.data[this.props.activeIndex]
     return (
       <div>
         <p>{tab.description}</p>
@@ -75,13 +71,62 @@ const Tabs = React.createClass({
 
 })
 
+const StatefulTabs = React.createClass({
+  getInitialState() {
+    return {
+      tabIndex: 0
+    }
+  },
+
+  tabChanged(tabIndex) {
+    this.setState({
+      tabIndex: tabIndex
+    })
+  },
+
+  render() {
+    return <Tabs {...this.props} onTabChange={this.tabChanged} activeIndex={this.state.tabIndex} />
+  }
+});
+
 const App = React.createClass({
+  getInitialState() {
+    return {
+      tabIndex: 0
+    }
+  },
+
+  tabChanged(tabIndex) {
+    this.setState({
+      tabIndex: tabIndex
+    })
+  },
+
+  decrementTab() {
+    this.setState({
+      tabIndex: this.state.tabIndex - 1
+    })
+  },
+
+  incrementTab() {
+    this.setState({
+      tabIndex: this.state.tabIndex + 1
+    })
+  },
 
   render() {
     return (
       <div>
         <h1>Props v. State</h1>
-        <Tabs ref="tabs" data={this.props.tabs} />
+
+        {this.state.tabIndex > 0 ? <button onClick={this.decrementTab}>rewind</button> : ''}<br />
+        {this.state.tabIndex < this.props.tabs.length - 1 ? <button onClick={this.incrementTab}>fast forward</button> : ''}
+
+        <h2>The stateless one (controllable above also)</h2>
+        <Tabs ref="tabs" data={this.props.tabs} onTabChange={this.tabChanged} activeIndex={this.state.tabIndex} />
+
+        <h2>The stateful one</h2>
+        <StatefulTabs ref="tabs" data={this.props.tabs} />
       </div>
     )
   }
